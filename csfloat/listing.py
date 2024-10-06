b9b53e13ac523c35a732705b73277e30e2496c3f
PaxxPatriot <34141101+PaxxPatriot@users.noreply.github.com>
2024-10-06 19:39:58 +0200

    Implement auction details

--- csfloat/listing.py
+++ csfloat/listing.py
@@ -23,20 +23,100 @@ SOFTWARE.
 """
 
 import datetime
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from .enums import ListingType
 from .item import Item, Reference
 from .user import User
 
-__all__ = ("Listing",)
+__all__ = ("AuctionDetails", "Listing",)
+
+
+class TopBid:
+    """Represents the top bid."""
+
+    __slots__ = (
+        "_bid_id",
+        "_created_at",
+        "_price",
+        "_contract_id",
+        "_state",
+        "_obfuscated_buyer_id",
+    )
+
+    def __init__(self, *, data: Dict[str, Any]) -> None:
+        self._bid_id = data.get("id", "")
+        self._created_at = data.get("created_at")
+        self._price = data.get("price", "")
+        self._contract_id = data.get("contract_id", 0)
+        self._state = data.get("state", 0)
+        self._obfuscated_buyer_id = data.get("obfuscated_buyer_id", 0)
+
+    @property
+    def bid_id(self):
+        return self._bid_id
+
+    @property
+    def created_at(self) -> datetime.datetime:
+        """:class:`datetime.datetime`: Returns the created at of the item."""
+        return datetime.datetime.fromisoformat(self._created_at)
+    
+    @property
+    def price(self) -> float:
+        """:class:`float`: Returns the price of the item in USD."""
+        return self._price / 100
+    
+    @property
+    def contract_id(self):
+        return self._contract_id
+    
+    @property
+    def state(self):
+        return self._state
+    
+    @property
+    def obfuscated_buyer_id(self):
+        return self._obfuscated_buyer_id
+
+
+class AuctionDetails:
+    """Represents the auction details."""
+
+    __slots__ = (
+        "_reserve_price",
+        "_top_bid",
+        "_expires_at",
+        "_min_next_bid",
+    )
+
+    def __init__(self, *, data: Dict[str, Any]) -> None:
+        self._reserve_price = data.get("reserve_price", 0.0)
+        self._top_bid = data.get("top_bid")
+        self._expires_at = data.get("expires_at", )
+        self._min_next_bid = data.get("min_next_bid", 0.0)
+
+    @property
+    def reserve_price(self):
+        return self._reserve_price / 100
+    
+    @property
+    def top_bid(self):
+        return TopBid(data=self._top_bid)
+    
+    @property
+    def expires_at(self):
+        return datetime.datetime.fromisoformat(self._expires_at)
+    
+    @property
+    def min_next_bid(self):
+        return self._min_next_bid / 100
 
 
 class Listing:
     """Represents a listing."""
 
     __slots__ = (
-        "_id",
+        "_listing_id",
         "_created_at",
         "_type",
         "_price",
@@ -47,10 +127,11 @@ class Listing:
         "_is_seller",
         "_is_watchlisted",
         "_watchers",
+        "_auction_details",
     )
 
     def __init__(self, *, data: Dict[str, Any]) -> None:
-        self._id = data.get("id", "")
+        self._listing_id = data.get("id", "")
         self._created_at = data.get("created_at")
         self._type = data.get("type", "")
         self._price = data.get("price", 0)
@@ -61,11 +142,12 @@ class Listing:
         self._is_seller = data.get("is_seller", False)
         self._is_watchlisted = data.get("is_watchlisted", False)
         self._watchers = data.get("watchers", 0)
+        self._auction_details = data.get("auction_details")
 
     @property
-    def id(self) -> str:
+    def listing_id(self) -> str:
         """:class:`str`: Returns the ID of the item."""
-        return self._id
+        return self._listing_id
 
     @property
     def created_at(self) -> datetime.datetime:
@@ -116,3 +198,7 @@ class Listing:
     def watchers(self) -> int:
         """:class:`int`: Returns the number of watchers for the item."""
         return self._watchers
+    
+    @property
+    def auction_details(self) -> Optional[AuctionDetails]:
+        return AuctionDetails(data=self._auction_details) if self._auction_details else None
