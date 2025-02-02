de846a69d136b82f06d980b5c60eff8a44c74407
PaxxPatriot <34141101+PaxxPatriot@users.noreply.github.com>
2025-02-02 11:27:10 +0100

    Bump version v0.2.0

--- csfloat/__init__.py
+++ csfloat/__init__.py
@@ -10,7 +10,7 @@ __title__ = "csfloat"
 __author__ = "PaxxPatriot"
 __license__ = "MIT"
 __copyright__ = "Copyright 2023-present PaxxPatriot"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
@@ -33,6 +33,6 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=0, minor=1, micro=0, releaselevel="final", serial=0)
+version_info: VersionInfo = VersionInfo(major=0, minor=2, micro=0, releaselevel="final", serial=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
